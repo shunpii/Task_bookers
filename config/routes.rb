06b4255e9a10605fda5a20808b3dfd0176371062
Shunya Sago <shun.s0029@gmail.com>
2021-07-26 11:34:30 +0000

    [Update] リンクの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
+
   get 'top' => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show',as:'book'
+  get 'books/:id/edit' => 'books#edit',as:'edit_book'
+  delete 'books/:id' => 'books#destroy'
+  patch 'books/:id' => 'books#update'
 
 end
