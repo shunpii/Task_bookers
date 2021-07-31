50fac5b6fb512d0e157234b79e2316333922a5b7
Shunya Sago <shun.s0029@gmail.com>
2021-07-31 12:08:44 +0000

    -a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all.order(created_at: :desc)
+    @books = Book.all
     @book = Book.new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/' => 'homes#top'
+
+  get 'top' => 'homes#top'
+  root to: 'homes#top'
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
+  get 'books' => 'books#index',as: 'index_book'
   get 'books/:id' => 'books#show',as:'book'
   get 'books/:id/edit' => 'books#edit',as:'edit_book'
   delete 'books/:id' => 'books#destroy'
   patch 'books/:id' => 'books#update'
+
+
 
 end
